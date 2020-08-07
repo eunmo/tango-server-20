b238fdc5714a2a7d09870823af83d3594517e10e
eunmo <eunmo@cs.stanford.edu>
2020-08-08 00:50:21 +0900

    use LinearProgress

--- src/summary.jsx
+++ src/summary.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
-import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import Paper from '@material-ui/core/Paper';
 import { deepOrange } from '@material-ui/core/colors';
 
@@ -108,7 +109,7 @@ export default () => {
   };
 
   if (langs.length === 0) {
-    return null;
+    return <LinearProgress />;
   }
 
   return (
