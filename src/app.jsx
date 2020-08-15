547bca6755d4b5fd267b5923c36fa07d530c2a13
eunmo <eunmo@cs.stanford.edu>
2020-08-15 17:26:02 +0900

    support dark mode

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,9 @@ import {
   Switch,
 } from 'react-router-dom';
 import Container from '@material-ui/core/Container';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import AppBar from './app-bar';
 import Search from './searchRoute';
@@ -13,28 +16,45 @@ import Edit from './edit';
 import Add from './add';
 import Summary from './summary';
 
-export default () => (
-  <Router>
-    <AppBar />
-    <Container maxWidth="md" component="main">
-      <Switch>
-        <Route path="/search/:keyword">
-          <Search />
-        </Route>
-        <Route path="/search">
-          <Search />
-        </Route>
-        <Route path="/edit/:level/:index">
-          <Edit />
-        </Route>
-        <Route path="/add">
-          <Add />
-        </Route>
-        <Route path="/summary">
-          <Summary />
-        </Route>
-        <Redirect from="/" to="/search" exact />
-      </Switch>
-    </Container>
-  </Router>
-);
+export default () => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <AppBar />
+        <Container maxWidth="md" component="main">
+          <Switch>
+            <Route path="/search/:keyword">
+              <Search />
+            </Route>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route path="/edit/:level/:index">
+              <Edit />
+            </Route>
+            <Route path="/add">
+              <Add />
+            </Route>
+            <Route path="/summary">
+              <Summary />
+            </Route>
+            <Redirect from="/" to="/search" exact />
+          </Switch>
+        </Container>
+      </Router>
+    </ThemeProvider>
+  );
+};
