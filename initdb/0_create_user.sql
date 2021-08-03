e6bfa8036fb1d9635a92307f0adf5051b31e0ee6
eunmo <eunmo@cs.stanford.edu>
2021-08-03 14:45:03 +0000

    remove remnants

--- initdb/0_create_user.sql
+++ initdb/0_create_user.sql
@@ -1,5 +1,3 @@
 CREATE USER 'dummy_user'@'%' IDENTIFIED WITH mysql_native_password BY 'dummy_password';
 CREATE DATABASE tango;
 GRANT ALL PRIVILEGES ON tango.* TO 'dummy_user'@'%';
-CREATE DATABASE tangotest;
-GRANT ALL PRIVILEGES ON tangotest.* TO 'dummy_user'@'%';
